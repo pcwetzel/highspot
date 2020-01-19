8cb9523c5fc6a5adf6976ec3ee1c85753a163b2e
Pete Wetzel <pcwetzel@gmail.com>
2020-01-19 02:38:13 -0800

    Reorganization of files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './js/components/card/card'
+
+const data = {
+  name: "Legate Rikke",
+  rarity: "Legendary",
+  type: "Creature",
+  subtypes: [
+    "Nord"
+  ],
+  cost: 4,
+  power: 3,
+  health: 4,
+  set: {
+    id: "hos",
+    name: "Heroes of Skyrim",
+    _self: "https://api.elderscrollslegends.io/v1/sets/hos"
+  },
+  collectible: true,
+  soulSummon: 1200,
+  soulTrap: 400,
+  text: "When you summon an Imperial, summon a 1/1 Imperial Grunt in the other lane.",
+  attributes: [
+    "Willpower"
+  ],
+  unique: true,
+  imageUrl: "https://images.elderscrollslegends.io/hos/legate_rikke.png",
+  id: "d6a7f917460de639b0493925f098bf82e9b31b83"
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <Card {...data} />
+  </>);
 }
 
 export default App;
--- src/js/components/card/card.js
+++ src/js/components/card/card.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './card.scss';
 
-const Card = () => {
-  const { image, name, text, set, type } = this.props;
+const Card = (props) => {
+  const { image, name, text, set, type } = props;
   return (
     <section>
-      { image }
+      <div className='card-front'>
+      { image && <div className='image'><img src={image} alt={ name } /></div>}
       <dl>
-        { name && `<dt>Name:</dt><dd>${name}</dd>` }
-        { text && `<dt>Text:</dt><dd>${text}</dd>` }
-        { set && `<dt>Set:</dt><dd>${set}</dd>` }
-        { type && `<dt>Type:</dt><dd>${type}</dd>` }
+        { name && <div><dt>Name:</dt><dd>{ name }</dd></div> }
+        { text && <div><dt>Text:</dt><dd>{ text }</dd></div> }
+        { set?.name && <div><dt>Set Name:</dt><dd>{ set.name }</dd></div> }
+        { type && <div><dt>Type:</dt><dd>{ type }</dd></div> }
       </dl>
+      </div>
     </section>
   )
 };
