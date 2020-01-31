dedc669edec6a64c8a84f87f0feb8577e8855406
Pete Wetzel <pete.wetzel@bluenile.com>
2020-01-30 16:26:19 -0800

    Styling

--- src/js/components/cardCount/cardCount.js
+++ src/js/components/cardCount/cardCount.js
@@ -7,7 +7,8 @@ const CardCount = (props) => {
   const { totalCards } = props;
 
   return (
-    <section className='card-total'>{ totalCards.toLocaleString() } { totalCards === 1 ? 'card' : 'cards' } found</section>
+    <section className='card-total'>{ totalCards.toLocaleString() } { totalCards === 1 ? 'card' : 'cards' }<span className='medium-and-bigger'>  found</span>
+    </section>
   );
 };
 
